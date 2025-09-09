1d1a3aecfd960db541604cfb9351110f61071fb5
JKalonewolf <jayakumarkumar7783@gmail.com>
2025-09-09 19:30:16 +0530

    First commit

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Jayakumar",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is Jayakumar. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Jayakumar, and I am a passionate and dedicated programmer with a strong enthusiasm for web application development. I am a quick learner with a self-driven attitude and enjoy exploring new technologies while solving complex problems. With a broad interest across multiple development stacks, I aim to contribute to building innovative and open solutions for the web. I am open to job opportunities that align with my skills, interests, and career growth.",
   email: 'jayakumarkumar7783@gmail.com',
   phone: '6383552637',
   address: '2/109, Linganampatti,Koppakari (PT), Denkanikottai (DK), Krishnagiri (DT), Tamil Nadu, 635116.',
